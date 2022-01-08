1141ecf483ff4502c4a427d0517da40c82f37e1c
collins <collinsboniface1000@gmail.com>
2022-01-08 21:08:31 +0200

    Create a new java class that extends to SpringBootServletInitializer to override the SpringApplicationBuilder method that return the application

--- src/main/java/com/home/controllers/SyncApplication.java
+++ src/main/java/com/home/controllers/SyncApplication.java
@@ -1,2 +1,16 @@
-package com.home.controllers;public class SyncApplication {
+package com.home.controllers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class SyncApplication extends SpringBootServletInitializer {
+        @Override
+        protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+            return application.sources(SyncApplication.class);
+        }
+
+        public static void main(String[] args) {
+            SpringApplication.run(SyncApplication.class, args);
+        }
 }
