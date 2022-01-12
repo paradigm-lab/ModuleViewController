e58d0ebfb2b575e440225c888a24a2c381ae9973
collins <collinsboniface1000@gmail.com>
2022-01-12 10:39:47 +0200

    Added a new controller which is been mapped to the cricketbat url

---
+++ src/main/java/com/home/controllers/mom/BroController.java
@@ -0,0 +1,16 @@
+package com.home.controllers.mom;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class BroController {
+
+    @ResponseBody
+    @RequestMapping("/cricketbat")
+    public String giveCricketBat(){
+        return "Hey man.. this is your cricket bat";
+    }
+
+}
