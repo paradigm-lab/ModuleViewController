3f31e8c180f9a2d1fd78ee799ce1da4d819eb602
collins <collinsboniface1000@gmail.com>
2022-01-12 13:36:52 +0200

    Removed the response body annotation whereby now i want to return the web page view in my controller

--- src/main/java/com/home/controllers/mom/BroController.java
+++ src/main/java/com/home/controllers/mom/BroController.java
@@ -2,12 +2,10 @@ package com.home.controllers.mom;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class BroController {
 
-    @ResponseBody
     @RequestMapping("/cricketbat")
     public String giveCricketBat(){
         return "Hey man.. this is your cricket bat";
