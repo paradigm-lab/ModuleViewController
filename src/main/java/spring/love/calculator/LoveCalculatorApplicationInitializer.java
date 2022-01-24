fbbe1785f750460d88f7570287f5b882d72ff85e
collins <collinsboniface1000@gmail.com>
2022-01-24 19:27:44 +0200

    Created the dispatcherServlet object and webApplicationContext object whereby we passed the webApplicationContext object in the DispatcherServlet constructor

--- src/main/java/spring/love/calculator/LoveCalculatorApplicationInitializer.java
+++ src/main/java/spring/love/calculator/LoveCalculatorApplicationInitializer.java
@@ -1,6 +1,8 @@
 package spring.love.calculator;
 
 import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.XmlWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -10,7 +12,13 @@ public class LoveCalculatorApplicationInitializer implements WebApplicationIniti
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
 
+        XmlWebApplicationContext webApplicationContext = new XmlWebApplicationContext();
+        webApplicationContext.setConfigLocation("classpath:application-config.xml");
+
+
         // Create a dispatcher servlet object
+        DispatcherServlet dispatcherServlet = new DispatcherServlet(webApplicationContext);
+
 
         // Register the dispatcher servlet with the servlet context object
 
