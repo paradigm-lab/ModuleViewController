f980ecccc45ed1f4537644406bfdde4e91ee0ae9
collins <collinsboniface1000@gmail.com>
2022-01-24 22:09:29 +0200

    Used the @Configuration and @ComponentScan to tell spring that this is my configuration class and scan for the basePackage

--- src/main/java/spring/love/calculator/config/LoveCalculatorAppConfig.java
+++ src/main/java/spring/love/calculator/config/LoveCalculatorAppConfig.java
@@ -1,4 +1,10 @@
 package spring.love.calculator.config;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "spring.love.calculator.controllers")
 public class LoveCalculatorAppConfig {
+
 }
