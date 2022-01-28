75a7325f7ebf08e3c9f4ae2061c99c9c5ceab279
collins <collinsboniface1000@gmail.com>
2022-01-28 22:01:20 +0200

    Created a new java class that we will use to configure the dispatcher servlet and extend to an Abstract class

---
+++ src/main/java/spring/love/calculator/config/LCAppIntializer.java
@@ -0,0 +1,22 @@
+package spring.love.calculator.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class LCAppIntializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[0];
+    }
+}
