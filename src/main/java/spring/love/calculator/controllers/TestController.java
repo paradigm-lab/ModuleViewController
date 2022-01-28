9f94798ede2b5382b0d05344b2e5de1127edf7d3
Matt Quinn <matt@mattjquinn.com>
2022-01-28 09:26:37 +0530

    JSP: files must be in webapp/, not resources/

--- src/main/java/spring/love/calculator/controllers/TestController.java
+++ src/main/java/spring/love/calculator/controllers/TestController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class testController {
+public class TestController {
 
     @RequestMapping("/test")
     public String sayHello() {
