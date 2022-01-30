f6eb78cd31be268f3b6736371b269e60c9092dbe
collins <collinsboniface1000@gmail.com>
2022-01-30 20:49:36 +0200

    Created a new package called api and created a new class called DTO for the data transfer object or the POJO class

---
+++ src/main/java/spring/love/calculator/api/UserInfoDTO.java
@@ -0,0 +1,34 @@
+package spring.love.calculator.api;
+
+public class UserInfoDTO {
+    private String userName;
+    private String crushName;
+
+
+    /*
+        The getter and setter method
+     */
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getCrushName() {
+        return crushName;
+    }
+
+    public void setCrushName(String crushName) {
+        this.crushName = crushName;
+    }
+
+    @Override
+    public String toString() {
+        return "UserInfoDTO{" +
+                "userName='" + userName + '\'' +
+                ", crushName='" + crushName + '\'' +
+                '}';
+    }
+}
