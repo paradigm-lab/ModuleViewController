12dab064a39473138bbe8485d608c3d6b05d7289
collins <collinsboniface1000@gmail.com>
2022-01-30 20:35:04 +0200

    Created the model object for as to capcher the values or record and using the addAttribute method to pass the variable name

--- src/main/java/spring/love/calculator/controllers/LCAppController.java
+++ src/main/java/spring/love/calculator/controllers/LCAppController.java
@@ -1,6 +1,7 @@
 package spring.love.calculator.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -13,9 +14,13 @@ public class LCAppController {
     }
 
     @RequestMapping("/process-homepage")
-    public String showResultPage(@RequestParam("userName") String userName1, @RequestParam("crushName") String crushName1) {
+    public String showResultPage(@RequestParam("userName") String userName1, @RequestParam("crushName") String crushName1, Model model) {
         System.out.println("User name is: " + userName1);
         System.out.println("Crush name is: " + crushName1);
+
+        model.addAttribute("userName", userName1);
+        model.addAttribute("crushName", crushName1);
+
         return "result-page";
     }
 
