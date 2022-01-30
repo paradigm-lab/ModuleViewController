16a33ef14346583eba8847c1d65ad6fdc8a69785
collins <collinsboniface1000@gmail.com>
2022-01-30 20:27:20 +0200

    Updated teh process-homepage mapping method by Captchering the values from the URL and bind the in to our mappign and printing them to the console

--- src/main/java/spring/love/calculator/controllers/LCAppController.java
+++ src/main/java/spring/love/calculator/controllers/LCAppController.java
@@ -2,6 +2,7 @@ package spring.love.calculator.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LCAppController {
@@ -12,8 +13,9 @@ public class LCAppController {
     }
 
     @RequestMapping("/process-homepage")
-    public String showResultPage() {
-
+    public String showResultPage(@RequestParam("userName") String userName1, @RequestParam("crushName") String crushName1) {
+        System.out.println("User name is: " + userName1);
+        System.out.println("Crush name is: " + crushName1);
         return "result-page";
     }
 
