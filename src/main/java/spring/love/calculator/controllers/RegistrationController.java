5c9bb8cf23fab7efc55a93d19aac36343ce80cb3
collins <collinsboniface1000@gmail.com>
2022-01-31 20:14:09 +0200

    Used the simple approach to created the model object and the Registaration class or the DTO class using the @ModelAttribute annotation

--- src/main/java/spring/love/calculator/controllers/RegistrationController.java
+++ src/main/java/spring/love/calculator/controllers/RegistrationController.java
@@ -3,6 +3,7 @@ package spring.love.calculator.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import spring.love.calculator.api.UserRegistrationDTO;
 
@@ -10,11 +11,8 @@ import spring.love.calculator.api.UserRegistrationDTO;
 public class RegistrationController {
 
     @RequestMapping("/register")
-    public String showRegistrationPage(Model model) {
-        UserRegistrationDTO dto = new UserRegistrationDTO();
-
-        model.addAttribute("userReg", dto);
-
+    public String showRegistrationPage(@ModelAttribute("userReg") UserRegistrationDTO dto) {
+        dto.setName("Collins");
         return "user-registration-page";
     }
 
