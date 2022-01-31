2d77df63a1e9ae3f15cd19f4e1e64b60dbade18d
collins <collinsboniface1000@gmail.com>
2022-01-31 19:43:50 +0200

    Created a controller class for the registration page

---
+++ src/main/java/spring/love/calculator/controllers/RegistrationController.java
@@ -0,0 +1,16 @@
+package spring.love.calculator.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class RegistrationController {
+
+    @RequestMapping("/register")
+    public String showRegistrationPage() {
+
+        return "user-registration-page";
+    }
+
+}
