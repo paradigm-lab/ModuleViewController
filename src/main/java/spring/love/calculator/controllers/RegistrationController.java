eaa0b0ea8f538c3da4e910f31c120b10d1cdd7e4
collins <collinsboniface1000@gmail.com>
2022-01-31 20:03:58 +0200

    Created the model and the UserRegistration object into my controller and sent the class object and attributeName in the addAttribute method

--- src/main/java/spring/love/calculator/controllers/RegistrationController.java
+++ src/main/java/spring/love/calculator/controllers/RegistrationController.java
@@ -2,13 +2,18 @@ package spring.love.calculator.controllers;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import spring.love.calculator.api.UserRegistrationDTO;
 
 @Controller
 public class RegistrationController {
 
     @RequestMapping("/register")
-    public String showRegistrationPage() {
+    public String showRegistrationPage(Model model) {
+        UserRegistrationDTO dto = new UserRegistrationDTO();
+
+        model.addAttribute("userReg", dto);
 
         return "user-registration-page";
     }
