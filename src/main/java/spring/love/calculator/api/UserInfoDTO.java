f20b11897931a2630197c6a39ec3b3edf08e6b89
collins <collinsboniface1000@gmail.com>
2022-02-02 00:45:58 +0200

    Used the size annotation to have my min and max value to be passed in my form

--- src/main/java/spring/love/calculator/api/UserInfoDTO.java
+++ src/main/java/spring/love/calculator/api/UserInfoDTO.java
@@ -2,11 +2,13 @@ package spring.love.calculator.api;
 
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 public class UserInfoDTO {
 
     @NotBlank(message = " * Your Name can't be blank")
     @Min(value = 3, message = "* your Name should have at least 3 characters")
+    @Size(min = 3, max = 15, message = "* Your Name should have at least 3 Character")
     private String userName; // = "Mr x";
     private String crushName; //= "Miss y";
 
