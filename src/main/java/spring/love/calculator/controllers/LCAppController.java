5330d5a4828030adae998d386e50cdb912610447
collins <collinsboniface1000@gmail.com>
2022-02-02 00:11:47 +0200

    Got reade of the Sys out and Created a List of Object and use a loop to iterate through the errors

--- src/main/java/spring/love/calculator/controllers/LCAppController.java
+++ src/main/java/spring/love/calculator/controllers/LCAppController.java
@@ -3,12 +3,14 @@ package spring.love.calculator.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import spring.love.calculator.api.UserInfoDTO;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 public class LCAppController {
@@ -39,7 +41,10 @@ public class LCAppController {
     public String showResultPage(@Valid @ModelAttribute("userInfo") UserInfoDTO userInfoDTO, BindingResult result) {
 
         if (result.hasErrors()) {
-            System.out.println("My form has errors...");
+            List<ObjectError> allErrors = result.getAllErrors();
+            for (ObjectError temp : allErrors) {
+                System.out.println(temp);
+            }
 
             return "home-page";
         }
