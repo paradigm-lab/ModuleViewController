1cabc85731dfad8ee3f771973fd606ddf30561f1
collins <collinsboniface1000@gmail.com>
2022-02-02 18:54:35 +0200

    Created a phone class type to handle the country code and number for the Phone type in my ComunicationDTO class

---
+++ src/main/java/spring/love/calculator/api/Phone.java
@@ -0,0 +1,33 @@
+package spring.love.calculator.api;
+
+public class Phone {
+    private String countryCode;
+    private String userNumber;
+
+
+    /*
+            The getter and setter method for the:
+            countryCode and userNumber
+         */
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public String getUserNumber() {
+        return userNumber;
+    }
+
+    public void setUserNumber(String userNumber) {
+        this.userNumber = userNumber;
+    }
+
+    @Override
+    public String toString() {
+        return getCountryCode() + " " + getUserNumber();
+    }
+
+}
