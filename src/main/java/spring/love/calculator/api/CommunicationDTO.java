e90d0fc525f117abc80dd5080ad40ddc31de0fcf
collins <collinsboniface1000@gmail.com>
2022-02-02 18:25:11 +0200

    Added a new email property with the getter and setter method

--- src/main/java/spring/love/calculator/api/CommunicationDTO.java
+++ src/main/java/spring/love/calculator/api/CommunicationDTO.java
@@ -1,4 +1,16 @@
 package spring.love.calculator.api;
 
 public class CommunicationDTO {
+
+    private String email;
+
+
+    // Getter and Setter methods
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
