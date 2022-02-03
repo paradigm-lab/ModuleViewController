5c0d85aa18a7d1efbdbaa12175639b06c4176094
collins <collinsboniface1000@gmail.com>
2022-02-03 23:33:12 +0200

    Created the Getter and Setter method for my class properties

--- src/main/java/spring/love/calculator/api/billDTO.java
+++ src/main/java/spring/love/calculator/api/billDTO.java
@@ -2,7 +2,24 @@ package spring.love.calculator.api;
 
 public class billDTO {
 
+    // The class property
     private CreditCard card;
     private Amount amount;
 
+    // Getter and Setter method
+    public CreditCard getCard() {
+        return card;
+    }
+
+    public void setCard(CreditCard card) {
+        this.card = card;
+    }
+
+    public Amount getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Amount amount) {
+        this.amount = amount;
+    }
 }
