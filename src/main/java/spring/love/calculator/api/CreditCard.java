74c73a868258d486f764d624f525cdbc0fb4c183
collins <collinsboniface1000@gmail.com>
2022-02-03 23:26:29 +0200

    Created the class property and the getter and setter methods for the property

--- src/main/java/spring/love/calculator/api/CreditCard.java
+++ src/main/java/spring/love/calculator/api/CreditCard.java
@@ -1,4 +1,44 @@
 package spring.love.calculator.api;
 
 public class CreditCard {
+
+    // The Property or fields for my class
+    private Integer firstFourDigits;
+    private Integer secondFourDigits;
+    private Integer thirdFourDigits;
+    private Integer lastFourDigits;
+
+
+    // The getter and setter method
+    public Integer getFirstFourDigits() {
+        return firstFourDigits;
+    }
+
+    public void setFirstFourDigits(Integer firstFourDigits) {
+        this.firstFourDigits = firstFourDigits;
+    }
+
+    public Integer getSecondFourDigits() {
+        return secondFourDigits;
+    }
+
+    public void setSecondFourDigits(Integer secondFourDigits) {
+        this.secondFourDigits = secondFourDigits;
+    }
+
+    public Integer getThirdFourDigits() {
+        return thirdFourDigits;
+    }
+
+    public void setThirdFourDigits(Integer thirdFourDigits) {
+        this.thirdFourDigits = thirdFourDigits;
+    }
+
+    public Integer getLastFourDigits() {
+        return lastFourDigits;
+    }
+
+    public void setLastFourDigits(Integer lastFourDigits) {
+        this.lastFourDigits = lastFourDigits;
+    }
 }
