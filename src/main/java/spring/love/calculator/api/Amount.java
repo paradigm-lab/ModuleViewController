b4b686a68c64f6f3dacdff286fca8cb321ca69f7
collins <collinsboniface1000@gmail.com>
2022-02-03 23:31:25 +0200

    Created the class property and getter and setter method for my Amount class

--- src/main/java/spring/love/calculator/api/Amount.java
+++ src/main/java/spring/love/calculator/api/Amount.java
@@ -1,5 +1,28 @@
 package spring.love.calculator.api;
 
+import java.math.BigDecimal;
+
 public class Amount {
 
+    // The Class properties
+    private BigDecimal billAmount;
+    private String localeDefinition;
+
+
+    // Created the setter and Getter
+    public BigDecimal getBillAmount() {
+        return billAmount;
+    }
+
+    public void setBillAmount(BigDecimal billAmount) {
+        this.billAmount = billAmount;
+    }
+
+    public String getLocaleDefinition() {
+        return localeDefinition;
+    }
+
+    public void setLocaleDefinition(String localeDefinition) {
+        this.localeDefinition = localeDefinition;
+    }
 }
