0597a45f65cf85caeabf688d857f5a43cb6b78e5
collins <collinsboniface1000@gmail.com>
2022-02-04 15:56:37 +0200

    Used the @ModelAttribute annotation to create the model object by spring and created billDTO object and spring will inject the object at the runntime of the program

--- src/main/java/spring/love/calculator/controllers/BillController.java
+++ src/main/java/spring/love/calculator/controllers/BillController.java
@@ -3,12 +3,13 @@ package spring.love.calculator.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import spring.love.calculator.api.BillDTO;
 
 @Controller
 public class BillController {
 
     @RequestMapping("/bill")
-    public String showBillPage() {
+    public String showBillPage(@ModelAttribute("userBill") BillDTO billDTO) {
 
         return "bill-payment";
     }
