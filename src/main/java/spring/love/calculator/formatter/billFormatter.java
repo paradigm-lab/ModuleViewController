38270abba8292c33effc7af0c5507da340eadcec
collins <collinsboniface1000@gmail.com>
2022-02-06 00:08:13 +0200

    Created a new billFormatter class that implement the Formatter interface that has got two methods which are parse and print

--- src/main/java/spring/love/calculator/formatter/billFormatter.java
+++ src/main/java/spring/love/calculator/formatter/billFormatter.java
@@ -1,4 +1,19 @@
 package spring.love.calculator.formatter;
 
-public class billFormatter {
+import org.springframework.format.Formatter;
+import spring.love.calculator.api.BillDTO;
+
+import java.text.ParseException;
+import java.util.Locale;
+
+public class billFormatter implements Formatter<BillDTO> {
+    @Override
+    public BillDTO parse(String text, Locale locale) throws ParseException {
+        return null;
+    }
+
+    @Override
+    public String print(BillDTO object, Locale locale) {
+        return null;
+    }
 }
