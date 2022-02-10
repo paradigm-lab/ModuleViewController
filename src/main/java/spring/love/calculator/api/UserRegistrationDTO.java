a60afa1fd241f111c446591dc004b7081088cd3d
collins <collinsboniface1000@gmail.com>
2022-02-10 21:24:56 +0200

    Created a new field for the age and added the getter and setter method for the age property

--- src/main/java/spring/love/calculator/api/UserRegistrationDTO.java
+++ src/main/java/spring/love/calculator/api/UserRegistrationDTO.java
@@ -7,6 +7,9 @@ public class UserRegistrationDTO {
     private String countryName;
     private String[] hobbies;
     private String gender;
+
+    private int age;
+
     private CommunicationDTO communicationDTO;
 
     // The getter and setter
@@ -64,5 +67,13 @@ public class UserRegistrationDTO {
 
     public void setCommunicationDTO(CommunicationDTO communicationDTO) {
         this.communicationDTO = communicationDTO;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 }
