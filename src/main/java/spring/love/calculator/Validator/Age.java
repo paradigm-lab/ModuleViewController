68d28c86cafc5fbd09cae629ab80c115b7c3ccc9
collins <collinsboniface1000@gmail.com>
2022-02-10 21:37:39 +0200

    Added the Documented Retention and Target annotation in the Age annotation for the validator

--- src/main/java/spring/love/calculator/Validator/Age.java
+++ src/main/java/spring/love/calculator/Validator/Age.java
@@ -1,4 +1,14 @@
 package spring.love.calculator.Validator;
 
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Documented
+@Retention(RUNTIME)
+@Target(FIELD)
 public @interface Age {
 }
