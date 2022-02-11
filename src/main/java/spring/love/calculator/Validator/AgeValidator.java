47ff791744071bb32445f775ebb2ec61a9c53f01
collins <collinsboniface1000@gmail.com>
2022-02-11 21:24:50 +0200

    Created the class property and assigned the lower and upper values to the properties

--- src/main/java/spring/love/calculator/Validator/AgeValidator.java
+++ src/main/java/spring/love/calculator/Validator/AgeValidator.java
@@ -5,9 +5,14 @@ import javax.validation.ConstraintValidatorContext;
 
 public class AgeValidator implements ConstraintValidator<Age, Integer> {
 
+    private int lower;
+    private int upper;
+
     @Override
-    public void initialize(Age constraintAnnotation) {
+    public void initialize(Age age) {
         // I can write the post construct work right here
+        this.lower = age.lower();
+        this.upper = age.upper();
     }
 
     @Override
