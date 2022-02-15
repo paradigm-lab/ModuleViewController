d821f162a7f6e172c498301ddbe871f5b85f42af
collins <collinsboniface1000@gmail.com>
2022-02-15 07:33:21 +0200

    Override the setAsText method to converte the string text into an upper case and setValue to the object

--- src/main/java/spring/love/calculator/propertyeditor/NamePropertyEditor.java
+++ src/main/java/spring/love/calculator/propertyeditor/NamePropertyEditor.java
@@ -3,4 +3,9 @@ package spring.love.calculator.propertyeditor;
 import java.beans.PropertyEditorSupport;
 
 public class NamePropertyEditor extends PropertyEditorSupport {
+    @Override
+    public void setAsText(String text) throws IllegalArgumentException {
+        String myConvertedStringValue = text.toUpperCase();
+        setValue(myConvertedStringValue);
+    }
 }
