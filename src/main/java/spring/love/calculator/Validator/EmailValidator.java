214f999acdd6391ebeb611d6a9349d3da1411b80
collins <collinsboniface1000@gmail.com>
2022-02-15 22:29:47 +0200

    Created a class and implement the Validator method whereby we will override the support and validate method in my class

---
+++ src/main/java/spring/love/calculator/Validator/EmailValidator.java
@@ -0,0 +1,17 @@
+package spring.love.calculator.Validator;
+
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+public class EmailValidator implements Validator {
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return false;
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+
+    }
+}
