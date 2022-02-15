d5050aafabc78a3153e9b4b93ab15bd98e87a2df
collins <collinsboniface1000@gmail.com>
2022-02-15 22:49:49 +0200

    Getting the email and convert to UserRegistrationDTO object and having a condition to check at the endof the String

--- src/main/java/spring/love/calculator/Validator/EmailValidator.java
+++ src/main/java/spring/love/calculator/Validator/EmailValidator.java
@@ -17,5 +17,13 @@ public class EmailValidator implements Validator {
     public void validate(Object object, Errors errors) {
         ValidationUtils.rejectIfEmptyOrWhitespace(errors, "communicationDTO.Email", "email.empty");
 
+        // Whether the email is ending with gmail.com
+        String email = ((UserRegistrationDTO)object).getCommunicationDTO().getEmail();
+
+        // Creating the condition statement
+        if(!email.endsWith("gmail.com")) {
+            errors.rejectValue("communicationDTO.email", "email.invalidDomain");
+        }
+
     }
 }
