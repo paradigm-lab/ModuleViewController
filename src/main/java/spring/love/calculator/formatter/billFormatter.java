15c850e65d156331f308d4c990d5c28c63cb7aa1
collins <collinsboniface1000@gmail.com>
2022-02-15 06:02:35 +0200

    Changed the Class type for the Formatter to CreditCard

--- src/main/java/spring/love/calculator/formatter/billFormatter.java
+++ src/main/java/spring/love/calculator/formatter/billFormatter.java
@@ -2,18 +2,20 @@ package spring.love.calculator.formatter;
 
 import org.springframework.format.Formatter;
 import spring.love.calculator.api.BillDTO;
+import spring.love.calculator.api.CreditCard;
 
 import java.text.ParseException;
 import java.util.Locale;
 
-public class billFormatter implements Formatter<BillDTO> {
+public class billFormatter implements Formatter<CreditCard> {
+
     @Override
-    public BillDTO parse(String text, Locale locale) throws ParseException {
+    public CreditCard parse(String text, Locale locale) throws ParseException {
         return null;
     }
 
     @Override
-    public String print(BillDTO object, Locale locale) {
+    public String print(CreditCard object, Locale locale) {
         return null;
     }
 }
