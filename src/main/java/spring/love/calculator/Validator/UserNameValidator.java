c34bf4249946d10807f03515fe2d4eb499be1a7e
collins <collinsboniface1000@gmail.com>
2022-02-15 21:49:52 +0200

    Updated the supports method by adding the dto class and used the equals method and added our logic in the validate method

--- src/main/java/spring/love/calculator/Validator/UserNameValidator.java
+++ src/main/java/spring/love/calculator/Validator/UserNameValidator.java
@@ -1,19 +1,23 @@
 package spring.love.calculator.Validator;
 
 import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
+import spring.love.calculator.api.UserRegistrationDTO;
 
 public class UserNameValidator implements Validator {
 
     // Check if the UserNameValidator supports a given object
     @Override
     public boolean supports(Class<?> clazz) {
-        return false;
+
+        return UserRegistrationDTO.class.equals(clazz);
     }
 
     // We need to write our customer validation logic
     @Override
     public void validate(Object target, Errors errors) {
-
+        // To Check if the field is null
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "userName", "userName.empty", "User name can't be empty");
     }
 }
