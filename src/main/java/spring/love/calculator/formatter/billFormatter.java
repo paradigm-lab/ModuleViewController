0a14c4bcece4bfc70c172a3ec3b5f0d8bed5a87c
collins <collinsboniface1000@gmail.com>
2022-02-15 06:09:11 +0200

    Updated my parse method by splitting the values and store them into an array and we use the setter method for my dto to set the values into my creditCard class

--- src/main/java/spring/love/calculator/formatter/billFormatter.java
+++ src/main/java/spring/love/calculator/formatter/billFormatter.java
@@ -11,7 +11,22 @@ public class billFormatter implements Formatter<CreditCard> {
 
     @Override
     public CreditCard parse(String text, Locale locale) throws ParseException {
-        return null;
+
+        // Splitting the credit Card
+        String[] digitsArray = text.split("-");
+
+        // Setting credit card values
+        CreditCard creditCard = new CreditCard();
+
+        // Setting the values to the DTO class using the digitsArray
+        creditCard.setFirstFourDigits(Integer.valueOf(digitsArray[0]));
+        creditCard.setSecondFourDigits(Integer.valueOf(digitsArray[1]));
+        creditCard.setThirdFourDigits(Integer.valueOf(digitsArray[2]));
+        creditCard.setLastFourDigits(Integer.valueOf(digitsArray[3]));
+
+        // Returning the Credit card objects
+        return creditCard;
+
     }
 
     @Override
