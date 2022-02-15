7eb56784e0f5c5ce1cf62bd7b5810dfb2db48c48
collins <collinsboniface1000@gmail.com>
2022-02-15 09:33:32 +0200

    Created a new package with a Credit card Coverter class with the source and Type of the converter

---
+++ src/main/java/spring/love/calculator/converters/CreditCardConverter.java
@@ -0,0 +1,7 @@
+package spring.love.calculator.converters;
+
+import org.springframework.core.convert.converter.Converter;
+import spring.love.calculator.api.CreditCard;
+
+public class CreditCardConverter implements Converter<String, CreditCard> {
+}
