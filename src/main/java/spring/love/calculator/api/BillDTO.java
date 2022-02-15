b0a29be9b6c48dee534fbd6044569a63423663fb
collins <collinsboniface1000@gmail.com>
2022-02-15 06:00:45 +0200

    Changed the class properties Classes and added the new Class property for the BillDTO class

--- src/main/java/spring/love/calculator/api/BillDTO.java
+++ src/main/java/spring/love/calculator/api/BillDTO.java
@@ -1,12 +1,18 @@
 package spring.love.calculator.api;
 
+import java.math.BigDecimal;
+import java.util.Currency;
+import java.util.Date;
+
 public class BillDTO {
 
     // The class property
     private CreditCard card;
-    private Amount amount;
+    private Date date;
+    private Currency currency;
+    private BigDecimal amount;
 
-    // Getter and Setter method
+    // Getter and setter Methods
     public CreditCard getCard() {
         return card;
     }
@@ -15,11 +21,27 @@ public class BillDTO {
         this.card = card;
     }
 
-    public Amount getAmount() {
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Currency getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(Currency currency) {
+        this.currency = currency;
+    }
+
+    public BigDecimal getAmount() {
         return amount;
     }
 
-    public void setAmount(Amount amount) {
+    public void setAmount(BigDecimal amount) {
         this.amount = amount;
     }
 }
