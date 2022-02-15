b597e1bab51e0b2cb833e18d7a28330e96d5ef3d
collins <collinsboniface1000@gmail.com>
2022-02-15 09:01:38 +0200

    Created my Credit Card Editor Class which extends the PropertyEditorSupport and Override the setAsText method

---
+++ src/main/java/spring/love/calculator/propertyeditor/CreditCardEditor.java
@@ -0,0 +1,10 @@
+package spring.love.calculator.propertyeditor;
+
+import java.beans.PropertyEditorSupport;
+
+public class CreditCardEditor extends PropertyEditorSupport {
+    @Override
+    public void setAsText(String text) throws IllegalArgumentException {
+        
+    }
+}
