ba73be55b2d55e4a260bbcabdc26fbbe7e10b8c2
collins <collinsboniface1000@gmail.com>
2022-02-15 09:07:52 +0200

    Added the logic to edit the property values and setting the value by passing the object

--- src/main/java/spring/love/calculator/propertyeditor/CreditCardEditor.java
+++ src/main/java/spring/love/calculator/propertyeditor/CreditCardEditor.java
@@ -1,10 +1,24 @@
 package spring.love.calculator.propertyeditor;
 
+import spring.love.calculator.api.CreditCard;
+
 import java.beans.PropertyEditorSupport;
 
 public class CreditCardEditor extends PropertyEditorSupport {
     @Override
     public void setAsText(String text) throws IllegalArgumentException {
-        
+        // Split the String values with the split method and store in the string array
+        String[] digitsArray = text.split("-");
+
+        // Setting Credit card Values
+        CreditCard creditCard = new CreditCard();
+
+        creditCard.setFirstFourDigits(Integer.valueOf(digitsArray[0]));
+        creditCard.setSecondFourDigits(Integer.valueOf(digitsArray[1]));
+        creditCard.setThirdFourDigits(Integer.valueOf(digitsArray[2]));
+        creditCard.setLastFourDigits(Integer.valueOf(digitsArray[3]));
+
+        // Returning Credit card Object
+        setValue(creditCard);
     }
 }
