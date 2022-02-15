5e3afb559fe6c1c769f33783027fd5e642ef6895
collins <collinsboniface1000@gmail.com>
2022-02-15 22:40:36 +0200

    Added the support to check if the Validator is True or false and used the validationUtils to add and load the error from the properties file

--- src/main/java/spring/love/calculator/Validator/EmailValidator.java
+++ src/main/java/spring/love/calculator/Validator/EmailValidator.java
@@ -1,17 +1,21 @@
 package spring.love.calculator.Validator;
 
 import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
+import spring.love.calculator.api.UserRegistrationDTO;
 
 public class EmailValidator implements Validator {
 
     @Override
     public boolean supports(Class<?> clazz) {
-        return false;
+
+        return UserRegistrationDTO.class.equals(clazz);
     }
 
     @Override
-    public void validate(Object target, Errors errors) {
+    public void validate(Object object, Errors errors) {
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "communicationDTO.Email", "email.empty");
 
     }
 }
