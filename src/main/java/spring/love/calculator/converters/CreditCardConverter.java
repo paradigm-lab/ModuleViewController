bcc10f21999746ef14e75961cbfe1f17ffbcc54e
collins <collinsboniface1000@gmail.com>
2022-02-15 09:39:29 +0200

    Overrided the coverter method which will return the CreditCard Object and taking the string as an argument and add the logic to get and sett the digits

--- src/main/java/spring/love/calculator/converters/CreditCardConverter.java
+++ src/main/java/spring/love/calculator/converters/CreditCardConverter.java
@@ -4,4 +4,19 @@ import org.springframework.core.convert.converter.Converter;
 import spring.love.calculator.api.CreditCard;
 
 public class CreditCardConverter implements Converter<String, CreditCard> {
+    @Override
+    public CreditCard convert(String source) {
+        // Split the String values by using the hyphen and store into the String array
+        String[] digitsArray = source.split("-");
+
+        // Setting Credit card values
+        CreditCard creditCard = new CreditCard();
+
+        creditCard.setFirstFourDigits(Integer.valueOf(digitsArray[0]));
+        creditCard.setSecondFourDigits(Integer.valueOf(digitsArray[1]));
+        creditCard.setThirdFourDigits(Integer.valueOf(digitsArray[2]));
+        creditCard.setLastFourDigits(Integer.valueOf(digitsArray[3]));
+
+        return creditCard;
+    }
 }
