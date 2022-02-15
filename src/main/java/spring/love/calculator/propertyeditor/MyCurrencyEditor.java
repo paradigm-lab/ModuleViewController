91216b6007b1ba1c3d7cbdce985d143eb49ef3cd
collins <collinsboniface1000@gmail.com>
2022-02-15 08:27:55 +0200

    Created the Currency object and conveted the lower case to upper case

--- src/main/java/spring/love/calculator/propertyeditor/MyCurrencyEditor.java
+++ src/main/java/spring/love/calculator/propertyeditor/MyCurrencyEditor.java
@@ -1,9 +1,13 @@
 package spring.love.calculator.propertyeditor;
 
 import java.beans.PropertyEditorSupport;
+import java.util.Currency;
 
 public class MyCurrencyEditor extends PropertyEditorSupport {
 
-    
-
+    @Override
+    public void setAsText(String text) throws IllegalArgumentException {
+        Currency currency = Currency.getInstance(text.toUpperCase());
+        setValue(currency);
+    }
 }
