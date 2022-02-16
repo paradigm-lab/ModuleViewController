7ec924eb6e4e7a34a2151b4ca88f8c9eaf2c2017
collins <collinsboniface1000@gmail.com>
2022-02-16 12:12:44 +0200

    Passed the key value in the NotEmpty annotation to load the message from the property file whereby the property file pass the key value pair approach

--- src/main/java/spring/love/calculator/api/UserRegistrationDTO.java
+++ src/main/java/spring/love/calculator/api/UserRegistrationDTO.java
@@ -6,7 +6,7 @@ import javax.validation.constraints.NotEmpty;
 
 public class UserRegistrationDTO {
 
-    @NotEmpty(message = "* can not be empty")
+    @NotEmpty(message = "{string.notEmpty}")
     private String name;
 
     private String userName;
