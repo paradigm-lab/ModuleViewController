133879589e3e67f13edddafc38d541c3fffdb59e
collins <collinsboniface1000@gmail.com>
2022-02-17 08:34:49 +0200

    Updated the Request mapping URL to be dynamically and used the PathVariable and addAttribute

--- src/main/java/spring/love/calculator/controllers/EmailController.java
+++ src/main/java/spring/love/calculator/controllers/EmailController.java
@@ -1,15 +1,21 @@
 package spring.love.calculator.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import spring.love.calculator.api.EmailDTO;
 
 @Controller
 public class EmailController {
 
-    @RequestMapping("/sendEmail")
-    public String sendEmail(@ModelAttribute("emailDTO") EmailDTO emailDTO) {
+    @RequestMapping("/sendEmail/{userName}")
+    public String sendEmail(@PathVariable("userName") String userName, EmailDTO emailDTO, Model model) {
+
+        model.addAttribute("emailDTO", new EmailDTO());
+
+        model.addAttribute("userName", "userName.toUpperCase()");
 
         return "send-email-page";
     }
