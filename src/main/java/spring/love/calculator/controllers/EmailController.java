e4ab1c83338ecdce0b5583f10fde56cfae0dc9d6
collins <collinsboniface1000@gmail.com>
2022-02-17 05:26:38 +0200

    Passed the model Attribute and the DTO class in my controller method

--- src/main/java/spring/love/calculator/controllers/EmailController.java
+++ src/main/java/spring/love/calculator/controllers/EmailController.java
@@ -1,13 +1,15 @@
 package spring.love.calculator.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import spring.love.calculator.api.EmailDTO;
 
 @Controller
 public class EmailController {
 
     @RequestMapping("/sendEmail")
-    public String sendEmail() {
+    public String sendEmail(@ModelAttribute("emailDTO") EmailDTO emailDTO) {
 
         return "send-email-page";
     }
