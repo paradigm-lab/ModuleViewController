340a1993924aee1e8a1460b6b431277b320df3eb
collins <collinsboniface1000@gmail.com>
2022-02-17 05:25:08 +0200

    Created a EmailDTO class for the email with one property and the getter and setter method

---
+++ src/main/java/spring/love/calculator/api/EmailDTO.java
@@ -0,0 +1,14 @@
+package spring.love.calculator.api;
+
+public class EmailDTO {
+    private String userEmail;
+
+    // Getter and setter method for the property
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+}
