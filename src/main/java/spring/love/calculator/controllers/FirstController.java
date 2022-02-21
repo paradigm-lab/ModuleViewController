d9ba140808e79d394985552f6bf82a7b50ebf526
collins <collinsboniface1000@gmail.com>
2022-02-21 22:44:49 +0200

    created a model object and used the addAttribute method to add the key and value to be passed into my view

--- src/main/java/spring/love/calculator/controllers/FirstController.java
+++ src/main/java/spring/love/calculator/controllers/FirstController.java
@@ -1,13 +1,17 @@
 package spring.love.calculator.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class FirstController {
 
     @RequestMapping("/first")
-    public String handlingMethod1() {
+    public String handlingMethod1(Model model) {
+
+        model.addAttribute("firstName", "Collins");
+        model.addAttribute("lastName", "Boniface");
 
         return "index";
     }
