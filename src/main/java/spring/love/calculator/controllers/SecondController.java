e97f42eb6b117a285a851bc19ad3feacf2711f8c
collins <collinsboniface1000@gmail.com>
2022-02-22 21:52:11 +0200

    Created a HttpSession object into my new controller and getAttribute from my HttpSession and pass it into my model

--- src/main/java/spring/love/calculator/controllers/SecondController.java
+++ src/main/java/spring/love/calculator/controllers/SecondController.java
@@ -5,11 +5,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttribute;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
 public class SecondController {
 
     @RequestMapping("/normalMethod")
-    public String handlingMethod3(@SessionAttribute("firstName") String firstName, Model model) {
+    public String handlingMethod3(@SessionAttribute("firstName") String firstName, Model model, HttpSession session) {
 
         // String firstName = (String) model.getAttribute("firstName");
 
@@ -17,6 +19,11 @@ public class SecondController {
         model.addAttribute("firstName", newFirstName);
 
         System.out.println("Inside the new Controller method, Printing firstName: " + firstName);
+
+        // Retrieving the session Attribute from http session
+        String address = (String) session.getAttribute("address");
+        String newAddress = address + " South Africa";
+        model.addAttribute("address", newAddress);
 
         return "index";
     }
