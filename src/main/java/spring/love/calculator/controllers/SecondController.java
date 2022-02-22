6a9a1baf367cef950f240020d8ae18f8a87f7bce
collins <collinsboniface1000@gmail.com>
2022-02-22 21:23:59 +0200

    Using the SessionAttribute to access the attribute from the Session in my new controller

--- src/main/java/spring/love/calculator/controllers/SecondController.java
+++ src/main/java/spring/love/calculator/controllers/SecondController.java
@@ -3,14 +3,19 @@ package spring.love.calculator.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 @Controller
 public class SecondController {
 
     @RequestMapping("/normalMethod")
-    public String handlingMethod3(Model model) {
+    public String handlingMethod3(@SessionAttribute("firstName") String firstName, Model model) {
 
-        String firstName = (String) model.getAttribute("firstName");
+        // String firstName = (String) model.getAttribute("firstName");
+
+        String newFirstName = "Mr " + firstName;
+        model.addAttribute("firstName", newFirstName);
+
         System.out.println("Inside the new Controller method, Printing firstName: " + firstName);
 
         return "index";
