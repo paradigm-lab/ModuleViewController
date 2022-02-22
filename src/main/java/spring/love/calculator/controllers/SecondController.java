8ee1581ba1048a78c9b07a9a037ce9dbc513b927
collins <collinsboniface1000@gmail.com>
2022-02-22 21:05:05 +0200

    Created the model object and getting the Attribute from my FirstController

--- src/main/java/spring/love/calculator/controllers/SecondController.java
+++ src/main/java/spring/love/calculator/controllers/SecondController.java
@@ -1,13 +1,17 @@
 package spring.love.calculator.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class SecondController {
 
     @RequestMapping("/normalMethod")
-    public String handlingMethod3() {
+    public String handlingMethod3(Model model) {
+
+        String firstName = (String) model.getAttribute("firstName");
+        System.out.println("Inside the new Controller method, Printing firstName: " + firstName);
 
         return "index";
     }
