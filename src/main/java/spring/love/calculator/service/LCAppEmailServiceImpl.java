ea3672fa6678b77f7b84530d7097f1333d1b5cef
collins <collinsboniface1000@gmail.com>
2022-02-23 17:00:21 +0200

    Fixed the dependency bug to be injected

--- src/main/java/spring/love/calculator/service/LCAppEmailServiceImpl.java
+++ src/main/java/spring/love/calculator/service/LCAppEmailServiceImpl.java
@@ -2,14 +2,14 @@ package spring.love.calculator.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LCAppEmailServiceImpl implements LCAppEmailService{
 
     @Autowired
-    private JavaMailSenderImpl javaMailSender;
+    private JavaMailSender javaMailSender;
 
     @Override
     public void sendEmail(String userName, String userEmail, String result) {
