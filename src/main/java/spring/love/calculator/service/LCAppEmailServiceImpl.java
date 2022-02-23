78c8d6f4ab11a995c73303acf9484bd9c58099fe
collins <collinsboniface1000@gmail.com>
2022-02-23 16:37:48 +0200

    Created the JavaMailSenderImpl object and send the simple mail object

--- src/main/java/spring/love/calculator/service/LCAppEmailServiceImpl.java
+++ src/main/java/spring/love/calculator/service/LCAppEmailServiceImpl.java
@@ -1,8 +1,15 @@
 package spring.love.calculator.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.stereotype.Service;
 
+@Service
 public class LCAppEmailServiceImpl implements LCAppEmailService{
+
+    @Autowired
+    private JavaMailSenderImpl javaMailSender;
 
     @Override
     public void sendEmail(String userName, String userEmail, String result) {
@@ -10,6 +17,8 @@ public class LCAppEmailServiceImpl implements LCAppEmailService{
         simpleMailMessage.setTo(userEmail);
         simpleMailMessage.setSubject("Love Calculator Application Result");
         simpleMailMessage.setText("Hi" + userName + " The result predicated by the LCApp is " + result);
+
+        javaMailSender.send(simpleMailMessage);
     }
 
 }
