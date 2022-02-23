b55b06d24d6294838a66f8947e34c732df729d66
collins <collinsboniface1000@gmail.com>
2022-02-23 16:13:12 +0200

    Created a new interface with the signature which takes the username userEmail and result

---
+++ src/main/java/spring/love/calculator/service/LCAppEmailServiceImpl.java
@@ -0,0 +1,15 @@
+package spring.love.calculator.service;
+
+import org.springframework.mail.SimpleMailMessage;
+
+public class LCAppEmailServiceImpl implements LCAppEmailService{
+
+    @Override
+    public void sendEmail(String userName, String userEmail, String result) {
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setTo(userEmail);
+        simpleMailMessage.setSubject("Love Calculator Application Result");
+        simpleMailMessage.setText("Hi" + );
+    }
+
+}
