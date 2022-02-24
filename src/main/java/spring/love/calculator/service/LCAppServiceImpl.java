1138273923e4d9b269e729b60b8539fc9e4c3c1d
collins <collinsboniface1000@gmail.com>
2022-02-24 19:59:44 +0200

    Overrided the method added the implementation of the methods

--- src/main/java/spring/love/calculator/service/LCAppServiceImpl.java
+++ src/main/java/spring/love/calculator/service/LCAppServiceImpl.java
@@ -4,8 +4,24 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class LCAppServiceImpl implements LCAppService{
+
+    public final String LC_APP_FORMULA = "FLAME";
+
     @Override
     public void calculateLove(String userName, String crushName) {
+        int userAndCrushNameCount = (userName+crushName).toCharArray().length;
+        int formulacount = LC_APP_FORMULA.toCharArray().length;
 
+        int rem = userAndCrushNameCount % formulacount;
+
+        char resultChar = LC_APP_FORMULA.charAt(rem);
+
+        String result = whatsBetweenUs(resultChar);
+    }
+
+    @Override
+    public String whatsBetweenUs(char calculationResult) {
+        
+        return null;
     }
 }
