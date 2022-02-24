fe4ffc1bbc59da08c70f47c16dfbcfcfe158b654
collins <collinsboniface1000@gmail.com>
2022-02-24 19:58:17 +0200

    Added a new method to calculate and return the result

--- src/main/java/spring/love/calculator/service/LCAppService.java
+++ src/main/java/spring/love/calculator/service/LCAppService.java
@@ -2,4 +2,5 @@ package spring.love.calculator.service;
 
 public interface LCAppService {
     void calculateLove(String userName, String crushName);
+    String whatsBetweenUs(char calculationResult);
 }
