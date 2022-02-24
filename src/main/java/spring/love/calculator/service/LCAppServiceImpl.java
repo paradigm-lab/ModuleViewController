abfa052941ea1f254728f561dc8dc923d9119616
collins <collinsboniface1000@gmail.com>
2022-02-24 19:50:14 +0200

    created a LCAppServiceImpl method that implement the LCAppService interface

---
+++ src/main/java/spring/love/calculator/service/LCAppServiceImpl.java
@@ -0,0 +1,11 @@
+package spring.love.calculator.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class LCAppServiceImpl implements LCAppService{
+    @Override
+    public void calculateLove(String userName, String crushName) {
+
+    }
+}
