d596b5f83eaa26e09bc3a2b8e1d1108874ed283b
collins <collinsboniface1000@gmail.com>
2022-02-28 22:50:57 +0300

    Created a new controller with one hande method

---
+++ src/main/java/spring/love/calculator/controllers/MyWebsiteController.java
@@ -0,0 +1,15 @@
+package spring.love.calculator.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MyWebsiteController {
+
+    @RequestMapping("/showInfo")
+    public String showWebsiteInfo() {
+
+        return "index";
+    }
+
+}
