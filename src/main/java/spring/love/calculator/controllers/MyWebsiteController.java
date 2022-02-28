5aabae27ad392e43555bc78c0eaf0de7855e1eac
collins <collinsboniface1000@gmail.com>
2022-02-28 23:14:17 +0300

    Set the values into my Properties using the setter method and passed my object into my model

--- src/main/java/spring/love/calculator/controllers/MyWebsiteController.java
+++ src/main/java/spring/love/calculator/controllers/MyWebsiteController.java
@@ -1,13 +1,21 @@
 package spring.love.calculator.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import spring.love.calculator.api.WebsiteInfoDTO;
 
 @Controller
 public class MyWebsiteController {
 
     @RequestMapping("/showInfo")
-    public String showWebsiteInfo() {
+    public String showWebsiteInfo(Model model) {
+
+        WebsiteInfoDTO websiteInfoDTO = new WebsiteInfoDTO();
+        websiteInfoDTO.setWebsiteName("kwawingu.com");
+        websiteInfoDTO.setWebsiteCategory("Cloud Services");
+
+        model.addAttribute("websiteInfo", websiteInfoDTO);
 
         return "index2";
     }
