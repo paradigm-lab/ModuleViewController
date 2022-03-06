76f6ea0309c4ada9600c0fd536a7cbfbb7cde51c
collins <collinsboniface1000@gmail.com>
2022-03-06 23:26:53 +0300

    Changed: To the Handler Method

--- src/main/java/spring/love/calculator/controllers/TestController.java
+++ src/main/java/spring/love/calculator/controllers/TestController.java
@@ -1,14 +1,22 @@
 package spring.love.calculator.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import spring.love.calculator.api.WebsiteInfoDTO;
 
 @Controller
 public class TestController {
 
     @RequestMapping("/test")
-    public String sayHello() {
-        return "helloWorld";
+    public String testModelAttribute(@ModelAttribute("website") WebsiteInfoDTO websiteInfoDTO) {
+        System.out.println("@RequestMapping : inside testModelAttribute()");
+
+        System.out.println(websiteInfoDTO.getWebsiteName());
+        System.out.println(websiteInfoDTO.getWebsiteCategory());
+
+
+        return "index";
     }
 
 }
