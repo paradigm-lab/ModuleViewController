fb16719ae808978e36ee1f5016afab0733a4ffbb
collins <collinsboniface1000@gmail.com>
2022-03-08 14:31:31 +0300

    Dependency creation

--- src/main/java/spring/love/calculator/controllers/TestController.java
+++ src/main/java/spring/love/calculator/controllers/TestController.java
@@ -3,10 +3,13 @@ package spring.love.calculator.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import spring.love.calculator.api.Info;
 import spring.love.calculator.api.WebsiteInfoDTO;
 
 @Controller
 public class TestController {
+
+    Info serverInfo;
 
     @RequestMapping("/test")
     public String testModelAttribute(@ModelAttribute("websiteInfo") WebsiteInfoDTO websiteInfoDTO) {
@@ -15,6 +18,8 @@ public class TestController {
         System.out.println(websiteInfoDTO.getWebsiteName());
         System.out.println(websiteInfoDTO.getWebsiteCategory());
 
+
+        System.out.println("Ip address of the Server is: " + serverInfo.getServerIp());
 
         return "index2";
     }
