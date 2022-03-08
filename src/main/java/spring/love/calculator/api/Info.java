bc0d3cd672a53092a49657c3374f9edcb380e2fe
collins <collinsboniface1000@gmail.com>
2022-03-08 14:23:11 +0300

    Created a new class with one property and setter and getter method

---
+++ src/main/java/spring/love/calculator/api/Info.java
@@ -0,0 +1,13 @@
+package spring.love.calculator.api;
+
+public class Info {
+    private String serverIp = "122:122:333:22";
+
+    public String getServerIp() {
+        return serverIp;
+    }
+
+    public void setServerIp(String serverIp) {
+        this.serverIp = serverIp;
+    }
+}
