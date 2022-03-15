478a0963f2faaa713c2d7eb5a59f337d7588fb44
collins <collinsboniface1000@gmail.com>
2022-03-15 21:23:24 +0300

    Created a new jsp page with the new controller with one handler method within the controller

---
+++ src/main/java/spring/love/calculator/controllers/MyController.java
@@ -0,0 +1,16 @@
+package spring.love.calculator.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MyController {
+
+    @RequestMapping("/")
+    public String showIndexPage() {
+
+        return "rootPage";
+    }
+
+}
---
+++ src/main/webapp/views/rootPage.jsp
@@ -0,0 +1,16 @@
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Root Page</title>
+
+</head>
+
+<body>
+
+    <h1>Root Page</h1>
+
+</body>
+</html>
