887ccf1df218b3a68e6104f310743c9ccab8c03c
collins <collinsboniface1000@gmail.com>
2022-03-15 21:27:33 +0300

    Updated the jsp page and fixed the RequestMapping

--- src/main/java/spring/love/calculator/controllers/MyController.java
+++ src/main/java/spring/love/calculator/controllers/MyController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MyController {
 
-    @RequestMapping("/")
+    @RequestMapping("/index")
     public String showIndexPage() {
 
         return "rootPage";
--- src/main/webapp/views/rootPage.jsp
+++ src/main/webapp/views/rootPage.jsp
@@ -10,7 +10,7 @@
 
 <body>
 
-    <h1>Root Page</h1>
+    <h1 align="center" >Root Page</h1>
 
 </body>
 </html>
